1c02eab14356dc248d92dc00641f304147245712
Granberg Borje <borje.granberg@saabgroup.com>
2016-01-20 11:17:09 +0100

    Read from stdin if there is anything to read

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/hex"
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"os"
@@ -10,14 +11,24 @@ import (
 
 var infile = flag.String("input", "", "The file to read")
 
+func readFromStdin() bool {
+	fi, _ := os.Stdin.Stat()
+	return fi.Size() > 0 || fi.Mode()&os.ModeNamedPipe != 0
+}
+
 func main() {
 	flag.Parse()
-	log.Println("file: ", *infile)
-
-	f, err := os.Open(*infile)
-	if err != nil {
-		log.Fatal("Failed to open ", *infile)
+	var f *os.File
+	if readFromStdin() {
+		f = os.Stdin
+	} else {
+		var err error
+		f, err = os.Open(*infile)
+		if err != nil {
+			log.Fatal("Failed to open ", *infile)
+		}
 	}
 	dumper := hex.Dumper(os.Stdout)
 	io.Copy(dumper, f)
+	f.Close()
 }
